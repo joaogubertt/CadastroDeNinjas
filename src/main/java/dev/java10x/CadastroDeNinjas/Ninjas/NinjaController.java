dd337737357e5bd4cc3042d6f7488fb12e9db922
joao.gubertt <joaogubertt@gmail.com>
2025-10-31 22:38:17 -0300

    fix: Padroniza o nome das rotas do controller dos ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -3,7 +3,7 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("ninja")
 public class NinjaController {
 
     @GetMapping("/boasvindas")
@@ -18,25 +18,25 @@ public class NinjaController {
     }
 
     //Mostrar todos os ninjas (READ)
-    @GetMapping("/todos")
+    @GetMapping("/listar")
     public String mostrarTodosOsNinjas(){
         return "Mostrar Ninja";
     }
 
     //Mostrar ninja por id (READ)
-    @GetMapping("/todosID")
+    @GetMapping("/listarID")
     public String mostrarNinjaPorID(){
         return "Mostrar Ninja Por ID";
     }
 
     //Alterar dados dos ninjas (UPDATE)
-    @PutMapping("/alterarID")
+    @PutMapping("/alterar")
     public String alterarNinjaPorID(){
         return "Alterar Ninja Por ID";
     }
 
     //Deletar Ninja (DELETE)
-    @DeleteMapping("/DeletarID")
+    @DeleteMapping("/deletar")
     public String deletarNinjaPorId(){
         return "Deletar Ninja Por ID";
     }
