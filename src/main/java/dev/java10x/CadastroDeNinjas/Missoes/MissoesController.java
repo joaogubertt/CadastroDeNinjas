47f031f0cf655db47400218ccf40f6960924904e
joao.gubertt <joaogubertt@gmail.com>
2025-11-03 21:30:46 -0300

    feat: Criando rotas no controller e regras de negócio no service para as missões

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -2,20 +2,34 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("missoes")
 public class MissoesController {
 
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService){
+        this.missoesService = missoesService;
+    }
+
     // GET -- Mandar uma requisição para mostrar as missões
     @GetMapping("/listar")
-    public String listarMissoes(){
-        return "Missões Listadas com sucesso";
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
     }
+
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarMissoesPorId(@PathVariable Long id){
+        return missoesService.listaMissoesPorId(id);
+    }
+
 
     // POST - Mandar uma requisição para criar as missões
     @PostMapping("/criar")
-    public String criarMissao() {
-        return "Missão Criada com sucesso";
+    public MissoesModel criarMissao(@RequestBody MissoesModel missao) {
+        return missoesService.criaMissoes(missao);
     }
 
 
@@ -26,8 +40,8 @@ public class MissoesController {
     }
 
     // DELETE - Mandar uma requisição para deletar as missões
-    @DeleteMapping("/deletar")
-    public String deletarMissao(){
-        return "Missão deletada com sucesso";
+    @DeleteMapping("/deletar/{id}")
+    public void deletarMissao(@PathVariable Long id){
+        missoesService.deletarMissaoPorId(id);
     }
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,40 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //Cria missão com entrada de missão do controller
+    public MissoesModel criaMissoes(MissoesModel missao){
+        return missoesRepository.save(missao);
+    }
+
+    //Lista todas as missões ao bater no endpoint
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    //Lista missão pelo id passado na controller
+    public MissoesModel listaMissoesPorId(Long id){
+        Optional<MissoesModel> missao = missoesRepository.findById(id);
+        return missao.orElse(null);
+    }
+
+    //Deleta missão pelo id passado na controller
+    public void deletarMissaoPorId(Long id){
+        missoesRepository.deleteById(id);
+    }
+
+    //Atualiza missão
+
 }
